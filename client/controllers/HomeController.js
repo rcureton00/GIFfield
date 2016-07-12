01ee0e4015887361193854310e1e4468b8fb0d4e
Reed Cureton <emailreedc@gmail.com>
2016-07-12 10:11:39 -0700

    MERGE player and chat FEAT

--- client/controllers/HomeController.js
+++ client/controllers/HomeController.js
@@ -1,89 +1,191 @@
-appPlayer.controller('HomeController', ['$scope', 'socket',
-  function($scope, socket) {
+appPlayer.controller('HomeController', ['$scope', 'socket', 'playerFactory'
+  function($scope, socket, playerFactory) {
      // Sound manager is a audio player library with hundreds of methods available,
      // The setup we have should be enough for a MVP.
-   SC.initialize({
-    client_id: '8af4a50e36c50437ca44cd59756301ae'
-   });
-   let track = '/tracks/291';
+    SC.initialize({
+      client_id: '8af4a50e36c50437ca44cd59756301ae'
+    });
+    let track = '/tracks/293';
+
     SC.stream(track, function(player){
-        $('#playBack').click(function(e) {
-            e.preventDefault();
-            player.start();
-            socket.emit("playNpause", track);
-        });
-        $('#stop').click(function(e) {
-            e.preventDefault();
-            player.pause();
-            socket.emit("playNpause", "pausing");
-        });
+      console.log('player', player);
+      playerFactory.player = player;
     });
 
-     /// chat controller stuff
+    $scope.play = function() {
+      console.log("player from fac", playerFactory.player);
+      if(playerFactory.player && !playerFactory.isPlaying) {
+        playerFactory.isPlaying = true;
+        playerFactory.player.play();
+        console.log('after play', playerFactory.player.id);
+        socket.emit("playNpause", {
+          id: playerFactory.player.id,
+          status: 'play'
+        });
+      }
+    }
 
-   $scope.chatSend = function(){
-   socket.emit('chat message', $scope.chatMsg);
-   $scope.chatMsg = "";
-   return false;
- }
+    $scope.pause = function() {
+      if(/*playerFactory.player &&*/ playerFactory.isPlaying) { 
+      console.log('player from fac', playerFactory.player);
+        playerFactory.isPlaying = false;
+        playerFactory.player.pause();
+        socket.emit("playNpause", {
+          id: playerFactory.player.id,
+          status: 'pause'
+        });
+      }
+    }
+
+
+    // SC.stream(track, function(player){
+    //   $('#playBack').click(function(e) {
+    //     e.preventDefault();
+    //     player.start();
+    //     socket.emit("playNpause", track);
+    //   });
+    //   $('#stop').click(function(e) {
+    //     e.preventDefault();
+    //     player.pause();
+    //     socket.emit("playNpause", "pausing");
+    //   });
+    // });
+
+  /// chat controller stuff
+
+    $scope.user = false;
+    $scope.typing = false;
+    $scope.TYPING_TIMER_LENGTH = 2000; // this is how quick the "[other user] is typing" message will go away
+    $scope.chatSend = function() {
+      socket.emit('chat message', $scope.chatMsg);
+      $scope.chatMsg = "";
+      return false;
+    }
  
- $scope.chatMessages = [];
+    $scope.chatMessages = [];
 
- socket.on('chat message', function(msg){
-   $scope.chatMessages.push(msg);
- });
- $scope.setName = function(){
-   //console.log($scope.screenName);
-   socket.emit('username', $scope.screenName);
- };
+    socket.on('chat message', function(msg){
+      $scope.chatMessages.push(msg);
+    });
+    
+    $scope.updateTyping = function() {
+      $scope.typing = true;
+      socket.emit('typing', userName.name);
+      var lastTypingTime = (new Date()).getTime();
+
+      setTimeout(function() {
+        var typingTimer = (new Date()).getTime();
+        var timeDiff = typingTimer - lastTypingTime;
+        if (timeDiff >= $scope.TYPING_TIMER_LENGTH && $scope.typing) {
+          socket.emit('stop typing');
+          $scope.typing = false;
+        }
+      }, $scope.TYPING_TIMER_LENGTH);
+    };
+
+    // Whenever the server emits 'typing', show the typing message
+    socket.on('typing', function(data) {
+
+      data.typing = true;
+      $scope.typingMessage = data.name + " is typing";
+
+      if (!$scope.chatMessages.includes($scope.typingMessage)) {
+          $scope.chatMessages.push($scope.typingMessage);
+      }
+    });
+
+    // Whenever the server emits 'stop typing', kill the typing message
+    socket.on('stop typing', function(data) {
+      data.typing = false;
+
+      var i = $scope.chatMessages.indexOf($scope.typingMessage);
+      $scope.chatMessages.splice(i, 1);
+    });
 
 
 
-//LISTENING --
+    //LISTENING --
 
-//PLAY WHEN HEARD from SOCKETS
-socket.on("playNpause", function(obj){
- console.log('we heard you', obj);
+    //PLAY WHEN HEARD from SOCKETS
+    socket.on("playNpause", function(obj){
+      console.log('we heard you', obj);
 
- SC.stream(obj, function(player){
-   player.start();
- });
- // if(obj === 'playing'){
- //  // musicInstance.playSound('http://users.skynet.be/fa046054/home/P22/track22.mp3', true);
+      if(!playerFactory.isPlaying && obj.status === "play"){
+        SC.stream('/tracks/293', function(player){
+          console.log('player', player);
+          playerFactory.player = player;
+          playerFactory.isPlaying = true;
+          playerFactory.player.play();
+        });
+      }
 
- // }
- // if(obj === 'pausing'){
- //   musicInstance.playSound.pause();
- // }
+      if(obj.status === "pause"){
+        playerFactory.player.pause();
+      }
 
+      // SC.stream(obj, function(player){
+      //   player.start();
+      // });
+      // if(obj === 'playing'){
+      //  // musicInstance.playSound('http://users.skynet.be/fa046054/home/P22/track22.mp3', true);
+
+      // }
+      // if(obj === 'pausing'){
+      //   musicInstance.playSound.pause();
+      // }
+
+    });
+  }
+])
+.controller('LandingPage', ['$scope', '$location', 'userName', 'socket', function($scope, $location, userName, socket) {
+  var name = '';
+  $scope.submit = function() {
+    if ($scope.text) {
+        name = userName.user(this.text);
+    }
+    socket.emit('username', userName.name);
+    $location.path('/home', false)
+  }
+}])
+.factory('playerFactory', function() {
+  var singleton = {};
+  singleton.player = null;
+  singleton.isPlaying = false;
+  return singleton;
+})
+.factory('socket', function($rootScope){
+  var socket = io.connect();
+   
+  return{
+    on: function(eventName, callback){
+      socket.on(eventName, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+          callback.apply(socket, args);
+        });
+      });
+    },
+
+    emit: function(eventName, data, callback){
+      socket.emit(eventName, data, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+          if(callback){
+            callback.apply(socket, args);
+          }
+        })
+      })
+    }
+  };
+})
+.factory('userName', function() {
+  var userSet = {};
+  userSet.name = '';
+  userSet.user = function(userVal) {
+      userSet.name = userVal;
+  };
+
+  return userSet;
 });
 
 
-}
-   
-])  
-.factory('socket', function($rootScope){
-   var socket = io.connect();
-   
- return{
-   on: function(eventName, callback){
-     socket.on(eventName, function(){
-       var args = arguments;
-       $rootScope.$apply(function(){
-       callback.apply(socket, args);
-       });
-     });
-   },
-
-     emit: function(eventName, data, callback){
-       socket.emit(eventName, data, function(){
-         var args = arguments;
-         $rootScope.$apply(function(){
-           if(callback){
-             callback.apply(socket, args);
-           }
-         })
-       })
-     }
-   };
- });
